50f6528138ae104353d6df96594fe1afaaeda648
nikitaprokopchuk <nikitapro@rambler.ru>
2017-09-13 21:56:23 +0300

    start graphs

--- ui/handler.go
+++ ui/handler.go
@@ -13,6 +13,9 @@ type moments struct {
 	From time.Time `json:"from"`
 	To   time.Time `json:"to"`
 }
+type jsonCurrencies struct {
+	Currencies [] db.Price
+}
 
 func Serve() {
 	fs := http.FileServer(http.Dir("ui/assets"))
@@ -32,22 +35,23 @@ func handleMomentRequests(rw http.ResponseWriter, req *http.Request) {
 		body, err := ioutil.ReadAll(req.Body)
 		if err != nil {
 			log.Printf("Failed to read request body! Error: %v\n", err)
-			http.Error(rw, "", http.StatusBadRequest)
+			http.Error(rw, "Failed to read request body!", http.StatusBadRequest)
 			return
 		}
 		m := moments{}
 		err = json.Unmarshal(body, &m)
 		if err != nil {
 			log.Printf("Failed to unmarshal request body! Error: %v\n", err)
-			http.Error(rw, "", http.StatusBadRequest)
+			http.Error(rw, "Failed to unmarshal request body!", http.StatusBadRequest)
 			return
 		}
-		currencies := db.RetrieveCurrencies(m.From, m.To)
-		if len(currencies) == 0 {
+		currs := db.RetrieveCurrencies(m.From, m.To)
+		if len(currs) == 0 {
 			http.NotFound(rw, req)
 			return
 		}
-		currenciesJson, err := json.Marshal(currencies)
+		jsCurrs := jsonCurrencies{Currencies: currs}
+		currenciesJson, err := json.Marshal(jsCurrs)
 		if err != nil {
 			log.Printf("Failed to marshal retrieved currencies! Error: %v\n", err)
 			http.Error(rw, "", http.StatusInternalServerError)
